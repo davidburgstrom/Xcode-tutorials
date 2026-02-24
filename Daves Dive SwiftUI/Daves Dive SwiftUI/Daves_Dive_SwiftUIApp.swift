2630e6a9c401a0bec6d237b6c070adc9942c9ee9
David Burgstrom <david.burgstrom@outlook.com>
2026-02-23 22:09:58 -0500

    Hello there world.

---
+++ Daves Dive SwiftUI/Daves Dive SwiftUI/Daves_Dive_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daves_Dive_SwiftUIApp.swift
+//  Daves Dive SwiftUI
+//
+//  Created by David Burgstrom on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Daves_Dive_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Daves Dive SwiftUI/Daves Dive SwiftUITests/Daves_Dive_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  Daves_Dive_SwiftUITests.swift
+//  Daves Dive SwiftUITests
+//
+//  Created by David Burgstrom on 2/23/26.
+//
+
+import Testing
+@testable import Daves_Dive_SwiftUI
+
+struct Daves_Dive_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
